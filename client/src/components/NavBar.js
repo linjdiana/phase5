c3cebc0522af8e058b2766280a19974a27633f6c
+ <+>
2023-04-03 15:58:43 -0700

    navbar formatting

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,27 +2,30 @@ import ChefsContainer from './ChefsContainer';
 import Calendar from './Calendar'
 import RecipesContainer from './RecipesContainer'
 import ReviewsContainer from './ReviewsContainer'
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
     return (
-        <>
-          <h1>navbar</h1>
-          {/* <Switch>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-          </Switch> */}
-        </>
+        <div class="navbar">
+          <div>currently testing in san francisco</div>
+          <p data-item='Foober'>Foober</p>
+
+          <section>
+            <div>Bringing the chef to you.</div>
+            <nav>
+              <ul class="menuItems">
+                <li><NavLink exact to="/chefs">Chefs</NavLink></li>
+                <li><NavLink exact to="/calendar">Calendar</NavLink></li>
+                <li><NavLink exact to="/recipes">Recipes</NavLink></li>
+                <li><NavLink exact to="/reviews">Reviews</NavLink></li>
+                <li><NavLink exact to="/contact">Contact</NavLink></li>
+              </ul>
+            </nav>
+
+          </section>
+        </div>
       );
 }
 
 export default NavBar
+
