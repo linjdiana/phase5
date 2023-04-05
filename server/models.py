5e99be4dbbf3da9c737319b4df0e66a0fefa3fe6
+ <+>
2023-04-04 17:32:56 -0700

    auth css

--- server/models.py
+++ server/models.py
@@ -12,29 +12,29 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String)
     _password_hash = db.Column(db.String)
 
-class Chef(db.Model, SerializerMixin):
-    __table__name = 'chefs'
-    id = db.Column(db.Integer, primary_key=True)
+# class Chef(db.Model, SerializerMixin):
+#     __table__name = 'chefs'
+#     id = db.Column(db.Integer, primary_key=True)
 
-    name=db.Column(db.String)
-    image=db.Column(db.String)
-    bio=db.Column(db.String)
+#     name=db.Column(db.String)
+#     image=db.Column(db.String)
+#     bio=db.Column(db.String)
 
-class Recipe(db.Model, SerializerMixin):
-    __table__name = 'recipes'
-    id = db.Column(db.Integer, primary_key=True)
+# class Recipe(db.Model, SerializerMixin):
+#     __table__name = 'recipes'
+#     id = db.Column(db.Integer, primary_key=True)
 
-    title=db.Column(db.String)
-    image=db.Column(db.String)
-    ingredients=db.Column(db.String)
-    Recipe=db.Column(db.String)
+#     title=db.Column(db.String)
+#     image=db.Column(db.String)
+#     ingredients=db.Column(db.String)
+#     Recipe=db.Column(db.String)
 
-class Review(db.Model, SerializerMixin):
-    __table__name = 'reviews'
-    id = db.Column(db.Integer, primary_key=True)
+# class Review(db.Model, SerializerMixin):
+#     __table__name = 'reviews'
+#     id = db.Column(db.Integer, primary_key=True)
 
-    user = db.Column(db.String)
-    chef_id = db.Column(db.Integer, db.ForeignKey('chefs.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    rating = db.Column(db.Integer)
-    text = db.Column(db.String)
+#     user = db.Column(db.String)
+#     chef_id = db.Column(db.Integer, db.ForeignKey('chefs.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     rating = db.Column(db.Integer)
+#     text = db.Column(db.String)
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,29 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, users, chefs, recipes, reviews
+from models import db, User, Chef, Review, Recipe
 
 if __name__ == '__main__':
     fake = Faker()
+
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        users = []
+        user1=User(name='Diana', email='linjdiana@gmail.com', _password_hash='')
+        users.append(user1)
+        db.session.add_all(users)
+        db.session.commit()
+
+
+        # chefs = []
+        # chef1 = Chef(name='Diana', image='', bio='pastries & chinese food')
+        # chef2 = Chef(name='Gordon', image='', bio='yelling at you')
+        # chef3 = Chef(name='Katya', image='', bio='italian')
+        # chef4 = Chef(name='Tony', image='', bio='traveled extensively so lots of variety.')
+        # chef5 = Chef(name='Amina', image='', bio='indian food & fresh californian cuisine, raised by a multicultural family')
+        # chefs.append(chef1)
+        # chefs.append(chef2)
+        # chefs.append(chef3)
+        # chefs.append(chef4)
+        # chefs.append(chef5)
+        # db.session.add_all(trainers)
+        # db.session.commit()
