1e4b0a7b0f873945f80ae5b1f479c446eb36ac01
+ <+>
2023-04-06 08:31:28 -0700

    auth complete

--- server/app.py
+++ server/app.py
@@ -1,16 +1,77 @@
-#!/usr/bin/env python3
+from flask import request, make_response, jsonify, session, abort
+from flask_restful import Resource, Api
+from werkzeug.exceptions import NotFound, Unauthorized
+from config import db, app, api
+from models import User
 
-# Standard library imports
+api = Api(app)
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+class Users(Resource):
+    def get(self):
+        user = User.query.filter_by(id=session['user_id']).first()
+        response = make_response(
+            user.to_dict(), 200
+        )
+        return response
+api.add_resource(Users, '/user')
 
-# Local imports
-from config import app, db, api
-from models import User, Recipe
+class Login(Resource):
+    def post(self):
+        try: 
+            user = User.query.filter_by(name=request.get_json()['name']).first()
+            if user.authenticate(request.get_json()['password']):
+                session['user_id'] = user.id
+                response = make_response(
+                    user.to_dict(),
+                    200
+                )
+                return response
+        except:
+            abort(401, "Incorrect Username or Password")
+api.add_resource(Login, '/login')
 
-# Views go here!
+class Signup(Resource):
+    def post(self):
+        form_json = request.get_json()
+        new_user = User(name=form_json['name'], email=form_json['email'], _password_hash=form_json['password'])
+        new_user.password_hash = form_json['password']
+        db.session.add(new_user)
+        db.session.commit()
+
+        response = make_response(
+            new_user.to_dict(),
+            201
+        )
+        return response
+api.add_resource(Signup, '/signup')
+
+class AuthorizedSession(Resource):
+    def get(self):
+        try: 
+            user = User.query.filter_by(id=session['user_id']).first()
+            response = make_response(
+                user.to_dict(),
+                200
+            )
+            return response
+        except:
+            abort(401, "Unauthorized")
+api.add_resource(AuthorizedSession, '/authorized')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        response = make_response('', 204)
+        return response
+api.add_resource(Logout, '/logout')
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+    response = make_response(
+        {"error": "Not Found: Sorry the resource you are looking for does not exist"},
+        404
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
